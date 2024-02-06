1d01f94d722fdebbcf55696f8342b6a417330d1d
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-02-06 23:51:45 +0700

    add horizontal card

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:green_circle/constants.dart';
 import 'package:green_circle/models/production.dart';
 import 'package:green_circle/screen/product_screen.dart';
+import 'package:badges/badges.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -18,74 +19,88 @@ class ProductCard extends StatelessWidget {
           ),
         );
       },
-      child:Stack(
-        children: [
-          Container(
-            height: 250,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              children: [
-                Image.asset(
-                  product.image,
-                  width: 120,
-                  height: 120,
-                ),
-                Text(
-                  product.title,
-                  style:titleProduct
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      "\$${product.price}",
-                      style: price
-                    ),
-                    Row(
-                      children: List.generate(
-                        product.colors.length,
-                            (cindex) => Container(
-                          height: 15,
-                          width: 15,
-                          margin: const EdgeInsets.only(right: 2),
-                          decoration: BoxDecoration(
-                            color: product.colors[cindex],
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
+      child:isHorizontal?Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Container(
+          height:160,
+          width: 160,
+          decoration: BoxDecoration(
+              border:Border.all(color:mediumGray,width: 2.0),
+              borderRadius:BorderRadius.circular(12),
           ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                width: 30,
-                height: 30,
-                decoration: const BoxDecoration(
-                  color: green1,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(10),
-                  ),
-                ),
-                child: const Icon(
-                  Icons.heart_broken,
-                  color: Colors.white,
-                  size: 18,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Hero(
+                tag: product.title,
+                child: Image.asset(
+                  product.image,
+                  width: 150,
+                  height: 95,
                 ),
               ),
-            ),
+              const Divider(),
+              Text(product.title,style:titleProduct,),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical:8,horizontal:10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("\$${product.price}",style:price,),
+                    IconButton(
+                        onPressed:(){},
+                        icon: const Icon(Icons.favorite_border_rounded)
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-        ],
+        ),
+      ):Padding(
+        padding: const EdgeInsets.only(left:10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color:mediumGray,width: 1.0),
+            boxShadow:[
+              BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 0,
+              blurRadius: 10,
+              offset: const Offset(2, 4),
+            )]
+          ),
+          child: Column(
+            children: [
+              Image.asset(
+                product.image,
+                width: 170,
+                height: 125,
+              ),
+              Text(product.title, style:titleProduct),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left:10.0),
+                      child: Text(
+                          "\$${product.price}",
+                          style: price
+                      ),
+                    ),
+                    IconButton(
+                        onPressed:(){},
+                        icon: Image.asset("assets/images/heart_icon.png",height:30,width:30,)
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
