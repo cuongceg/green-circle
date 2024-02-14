0ac811bbd5fdb05dccad41352933c6cecfa1f21e
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-02-14 22:52:37 +0700

    add new welcome page

---
+++ lib/screen/welcome_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:green_circle/screen/wrapper.dart';
+import 'package:animate_do/animate_do.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 1900), () {
+      // Navigate to the next screen after the delay
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Wrapper()),
+      );
+    });
+    double heightR=MediaQuery.of(context).size.height;
+    double widthR=MediaQuery.of(context).size.width;
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: const SystemUiOverlayStyle(
+          statusBarColor: Colors.white,
+        ),
+        child:Scaffold(
+          backgroundColor: Colors.white,
+          body: SizedBox(
+              width: widthR,
+              height: heightR,
+              child: Stack(
+                children:[
+                  Positioned(
+                    top: 100,
+                    left: 70,
+                    child: FadeInDownBig(
+                      delay: const Duration(milliseconds: 200),
+                      duration: const Duration(milliseconds: 1500),
+                      child: SizedBox(
+                        width: widthR /1.5,
+                        height: heightR /1.5,
+                        child: Center(
+                          child:Image.asset("assets/images/logo.png",),),
+                      ),
+                    ),
+                  ),
+                ],)
+          ),
+        )
+    );
+  }
+}
