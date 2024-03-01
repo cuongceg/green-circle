0af3a4a35964bb8828197b9980d0a11c3030be6d
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-03-01 19:50:06 +0700

    update UI

--- lib/screen/e_commerce/chat_bot.dart
+++ lib/screen/e_commerce/chat_bot.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:green_circle/constants.dart';
+import 'package:green_circle/screen/e_commerce/main_screen.dart';
 import 'package:green_circle/widgets/e_commerce/chat/chat_input_box.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
@@ -23,57 +24,70 @@ class _SectionStreamChatState extends State<SectionStreamChat> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-            child: chats.isNotEmpty
-                ? Align(
-              alignment: Alignment.bottomCenter,
-              child: SingleChildScrollView(
-                reverse: true,
-                child: ListView.builder(
-                  itemBuilder: chatItem,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: chats.length,
-                  reverse: false,
-                ),
-              ),
-            )
-                : Center(child: Text('Hello I am app support agency,\nhow can I help you ?',textAlign: TextAlign.center,style: title3Black,))),
-        if (loading) const CircularProgressIndicator(),
-        ChatInputBox(
-          controller: controller,
-          onSend: () {
-            if (controller.text.isNotEmpty) {
-              final searchedText = controller.text;
-              chats.add(
-                  Content(role: 'user', parts: [Parts(text: searchedText)]));
-              controller.clear();
-              loading = true;
-
-              gemini.streamChat(chats).listen((value) {
-                debugPrint("-------------------------------");
-                debugPrint("${value.output}");
-                loading = false;
-                setState(() {
-                  if (chats.isNotEmpty &&
-                      chats.last.role == value.content?.role) {
-                    chats.last.parts!.last.text =
-                    '${chats.last.parts!.last.text}${value.output}';
-                  } else {
-                    chats.add(Content(
-                        role: 'model', parts: [Parts(text: value.output)]));
-                  }
-                });
-              });
-            }
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon:const Icon(Icons.arrow_back),
+          onPressed:(){
+            Navigator.push(context,MaterialPageRoute(builder: (context)=>const MainScreen()));
           },
         ),
-      ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: chats.isNotEmpty
+                  ? Align(
+                alignment: Alignment.bottomCenter,
+                child: SingleChildScrollView(
+                  reverse: true,
+                  child: ListView.builder(
+                    itemBuilder: chatItem,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: chats.length,
+                    reverse: false,
+                  ),
+                ),
+              )
+                  : Center(child: Text(
+                'Hello I am app support agency,\nhow can I help you ?',
+                textAlign: TextAlign.center, style: title3Black,))),
+          if (loading) const CircularProgressIndicator(),
+          ChatInputBox(
+            controller: controller,
+            onSend: () {
+              if (controller.text.isNotEmpty) {
+                final searchedText = controller.text;
+                chats.add(
+                    Content(role: 'user', parts: [Parts(text: searchedText)]));
+                controller.clear();
+                loading = true;
+
+                gemini.streamChat(chats).listen((value) {
+                  debugPrint("-------------------------------");
+                  debugPrint("${value.output}");
+                  loading = false;
+                  setState(() {
+                    if (chats.isNotEmpty &&
+                        chats.last.role == value.content?.role) {
+                      chats.last.parts!.last.text =
+                      '${chats.last.parts!.last.text}${value.output}';
+                    } else {
+                      chats.add(Content(
+                          role: 'model', parts: [Parts(text: value.output)]));
+                    }
+                  });
+                });
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
-
   Widget chatItem(BuildContext context, int index) {
     final Content content = chats[index];
 
