3851c9895cb9f20eab7398d6d9a704296564c9db
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-05-17 23:53:20 +0700

    fire: remove UI of favourite products

--- lib/screen/e_commerce/like_product_screen.dart
+++ lib/screen/e_commerce/like_product_screen.dart
@@ -62,113 +62,80 @@ class _LikeProductScreenState extends State<LikeProductScreen> {
               child: IconButton(icon: const Icon(Icons.shopping_cart,size:25,), onPressed: () {}),
             )
           ],
-          bottom: TabBar(
-              labelColor:Colors.white,
-              labelStyle: label,
-              labelPadding: const EdgeInsets.symmetric(horizontal:10),
-              unselectedLabelColor: Colors.black,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicator: BoxDecoration(
-                  borderRadius: BorderRadius.circular(7),
-                  color:green4
-              ),
-              tabs: const [
-                Tab(
-                    child:Center(
-                      child:Text("All"),
-                    )
-                ),
-                Tab(
-                    child:Center(
-                      child:Text("Discount"),
-                    )
-                ),
-                Tab(
-                    child:Center(
-                      child:Text("Status"),
-                    )
-                ),
-              ]),
         ),
-        body:TabBarView(
+        body:ListView(
           children: [
-            ListView(
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
-                    childAspectRatio:0.95,
-                  ),
-                  itemCount: box.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(12),
-                            border: Border.all(color:mediumGray,width: 1.0),
-                            boxShadow:[
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.3),
-                                spreadRadius: 0,
-                                blurRadius: 10,
-                                offset: const Offset(2, 4),
-                              )]
+            const SizedBox(
+              height: 20,
+            ),
+            GridView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                childAspectRatio:0.95,
+              ),
+              itemCount: box.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color:mediumGray,width: 1.0),
+                        boxShadow:[
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 0,
+                            blurRadius: 10,
+                            offset: const Offset(2, 4),
+                          )]
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(box.getAt(index)!.imageUrl,
+                          width: 170,
+                          height: 100,
+                          fit: BoxFit.contain,
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        Padding(
+                          padding: const EdgeInsets.only(left:10,top: 0),
+                          child: Text(box.getAt(index)!.title,style:body1Black,),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Image.network(box.getAt(index)!.imageUrl,
-                              width: 170,
-                              height: 100,
-                              fit: BoxFit.contain,
-                            ),
                             Padding(
-                              padding: const EdgeInsets.only(left:10,top: 0),
-                              child: Text(box.getAt(index)!.title,style:body1Black,),
+                              padding:const EdgeInsets.only(left:10.0),
+                              child: Text("${box.getAt(index)!.price.toStringAsFixed(0)} 000",style:body1Black,),
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Padding(
-                                  padding:const EdgeInsets.only(left:10.0),
-                                  child: Text("${box.getAt(index)!.price.toStringAsFixed(0)} 000",style:body1Black,),
-                                ),
-                                TextButton.icon(
-                                    onPressed:(){
-                                      setState(() {
-                                        int favourNumbers= box.getAt(index)!.favorNumbers;
-                                        String productId= box.getAt(index)!.productId;
-                                        box.deleteAt(index);
-                                        // favourNumbers-=1;
-                                        Database().productionCollection.doc(productId).update({"likedNumber":favourNumbers});
-                                      });
-                                    },
-                                    icon: const Icon(Icons.favorite,color:Colors.red,),
-                                    label: Text("${box.getAt(index)!.favorNumbers}",style:body1Black,)),
-                              ],
-                            ),
+                            TextButton.icon(
+                                onPressed:(){
+                                  setState(() {
+                                    int favourNumbers= box.getAt(index)!.favorNumbers;
+                                    String productId= box.getAt(index)!.productId;
+                                    box.deleteAt(index);
+                                    // favourNumbers-=1;
+                                    Database().productionCollection.doc(productId).update({"likedNumber":favourNumbers});
+                                  });
+                                },
+                                icon: const Icon(Icons.favorite,color:Colors.red,),
+                                label: Text("${box.getAt(index)!.favorNumbers}",style:body1Black,)),
                           ],
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ],
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-            const Icon(Icons.movie),
-            const Icon(Icons.games),
           ],
-        ),
+        )
       ),
     );
   }
