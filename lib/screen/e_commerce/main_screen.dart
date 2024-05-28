9498c6771ec4c994665c1d8a15c2664d97f9e868
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-05-28 10:25:04 +0700

    feat: tutorials for user

--- lib/screen/e_commerce/main_screen.dart
+++ lib/screen/e_commerce/main_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:green_circle/constants.dart';
-import 'package:green_circle/screen/e_commerce/add_product.dart';
-import 'package:green_circle/screen/e_commerce/chat_screen.dart';
-import 'like_product_screen.dart';
+import 'package:green_circle/screen/mapbox/map.dart';
 import 'package:green_circle/screen/e_commerce/profile_screen.dart';
+import 'package:green_circle/screen/scan_screen.dart';
+import 'package:green_circle/screen/user%20shop/create_shop.dart';
 import 'package:green_circle/widgets/e_commerce/nav_bar.dart';
+import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -15,28 +17,139 @@ class MainScreen extends StatefulWidget {
 }
 class _MainScreenState extends State<MainScreen> {
   int currentTab = 0;
+  late TutorialCoachMark tutorialCoachMark;
+  GlobalKey homeButton=GlobalKey();
+  GlobalKey scanButton=GlobalKey();
+  GlobalKey gameButton=GlobalKey();
+  GlobalKey mapButton=GlobalKey();
+  GlobalKey profileButton=GlobalKey();
+  GlobalKey welcome=GlobalKey();
   List screens = const [
     NavBar(),
-    ChatScreen(),
-    AddProduct(),
-    LikeProductScreen(),
+    ScanScreen(),
+    ShopInfoScreen(),
+    FullMap(),
     MeScreen(),
   ];
+  @override
+  void initState() {
+    super.initState();
+    createUserGuide();
+    showUserGuide();
+  }
+  @override
+  void dispose() {
+    tutorialCoachMark.finish();
+    super.dispose();
+  }
+
+  void showUserGuide(){
+    tutorialCoachMark.show(context: context);
+  }
+  void createUserGuide(){
+    tutorialCoachMark=TutorialCoachMark(
+      targets: _createdTargets(),
+      colorShadow: greenGray,
+      textSkip: "SKIP",
+      textStyleSkip: snackBarFonts,
+      paddingFocus: 5,
+      opacityShadow: 0.5,
+      imageFilter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+    );
+  }
+
+  List<TargetFocus> _createdTargets(){
+    List<TargetFocus> targets = [];
+
+    targets.add(
+      TargetFocus(
+        identify: "homeButton",
+        keyTarget: homeButton,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.custom,
+            customPosition: CustomTargetContentPosition(bottom: -10,left:-50,right:00),
+            child: instruction("Đây là màn hình chính của ứng dụng, nơi mua sắm dành riêng cho bạn",450,250),
+          ),
+        ],
+      ),
+    );
+
+    targets.add(
+      TargetFocus(
+        identify: "scanButton",
+        keyTarget: scanButton,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        radius: 5,
+        contents: [
+          TargetContent(
+            align: ContentAlign.custom,
+            customPosition: CustomTargetContentPosition(bottom: -50,left:-50,right:0),
+            child: instruction("Hãy thử tính năng quét sản phẩm có thể tái chế không ở đây ",450,250),
+          ),
+        ],
+      ),
+    );
+
+    targets.add(
+      TargetFocus(
+        identify: "gameButton",
+        keyTarget: gameButton,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        radius: 5,
+        contents: [
+          TargetContent(
+            align: ContentAlign.custom,
+            customPosition: CustomTargetContentPosition(bottom: -110,left:-50,right:0),
+            child: instruction("Trò chơi phân loại rác thú vị nằm ở đây",450,250),
+          ),
+        ],
+      ),
+    );
+
+    targets.add(
+      TargetFocus(
+        identify: "mapButton",
+        keyTarget: mapButton,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        radius: 5,
+        contents: [
+          TargetContent(
+            align: ContentAlign.custom,
+            customPosition: CustomTargetContentPosition(bottom: -135,left:-50,right:0),
+            child: instruction("Theo dõi đơn hàng dễ dàng và tìm các địa điểm thu gom rác gần mình",450,250),
+          ),
+        ],
+      ),
+    );
+
+    targets.add(
+      TargetFocus(
+        identify: "profileButton",
+        keyTarget: profileButton,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        radius: 5,
+        contents: [
+          TargetContent(
+            align: ContentAlign.custom,
+            customPosition: CustomTargetContentPosition(bottom: -135,left:-50,right:0),
+            child: instruction("Đây là hồ sơ của bạn",450,250),
+          ),
+        ],
+      ),
+    );
+    return targets;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            currentTab = 2;
-          });
-        },
-        shape: const CircleBorder(),
-        backgroundColor: green4,
-        child: Image.asset("assets/images/scan_icon.png",height:30,width:30,color: Colors.white,)
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar:BottomAppBar(
         elevation: 0,
         height: 75,
@@ -49,16 +162,20 @@ class _MainScreenState extends State<MainScreen> {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            labelIcon(Icons.home_outlined,0,"Home"),
+            labelIcon(Icons.home_outlined,0,"Home",homeButton),
+            labelIcon(Icons.document_scanner_outlined ,1,"Scan",scanButton),
             Padding(
-              padding: currentTab==2?const EdgeInsets.only(left:15,right:50):const EdgeInsets.only(left:0,right:40),
-              child: labelIcon(Icons.support_agent,1,"Chat"),
+              padding: currentTab==2?const EdgeInsets.only(left:5,right:3.9):const EdgeInsets.only(left:2),
+              child: labelIcon(Icons.videogame_asset_outlined,2,"Game",gameButton),
             ),
             Padding(
-              padding: currentTab==2?const EdgeInsets.only(left:29,right:10):const EdgeInsets.only(left:29,right:0),
-              child: labelIcon(Icons.favorite_border_rounded,3,"Likes"),
+              padding: currentTab==3?const EdgeInsets.only(left:3,right: 5):const EdgeInsets.only(left:2),
+              child: labelIcon(Icons.map,3,"Map",mapButton),
             ),
-            labelIcon(Icons.person,4,"Profile"),
+            Padding(
+              padding:const EdgeInsets.only(left:1),
+              child: labelIcon(Icons.person,4,"Profile",profileButton),
+            ),
           ],
         ),
       ),
@@ -66,8 +183,9 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  Widget labelIcon(IconData icon,int selectedTab,String label){
+  Widget labelIcon(IconData icon,int selectedTab,String label,Key? key){
     return TextButton.icon(
+      key: key,
       onPressed:(){
         setState(() {
           currentTab = selectedTab;
@@ -82,6 +200,35 @@ class _MainScreenState extends State<MainScreen> {
         style:GoogleFonts.almarai(
             color:currentTab==selectedTab?Colors.white:mediumGray
         ),
+      ),
+    );
+  }
+
+  Widget instruction(String text,double width,double sWidth){
+    return Container(
+      height: 320,
+      width: width,
+      padding: const EdgeInsets.all(10),
+      child: Stack(
+        children:[
+          Image.asset('assets/images/dump.png',height: 200,width: 200,),
+          Positioned(
+            top:0,
+            right:0,
+            child: Container(
+              width: sWidth,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.only(topLeft: Radius.circular(15),topRight: Radius.circular(15),bottomRight: Radius.circular(15)),
+                border: Border.all(color: Colors.black87),
+              ),
+              child: Center(
+                child: Text(text, style: body1Black, textAlign: TextAlign.center,),
+              ),
+            ),
+          )
+        ]
       ),
     );
   }
