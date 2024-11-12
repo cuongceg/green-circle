caa782748b72fd7290e2380168f711d7a68e20c7
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-11-12 09:21:53 +0700

    refactor : change text to english

--- lib/screen/user shop/finish_sign_in.dart
+++ lib/screen/user shop/finish_sign_in.dart
@@ -18,168 +18,168 @@ class _FinishSignInState extends State<FinishSignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hoàn tất đăng ký'),
         backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back,color: Colors.black,),
-          onPressed: () {
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const ShopInfoScreen()));
-          },
-      ),),
-      body: ListView(
-        children: [
-          EasyStepper(
-            activeStep: activeStep,
-            lineStyle: const LineStyle(
-                lineLength: 80,
-                lineWidth: 2,
-                progressColor: green1,
-                progress: 0
+        appBar: AppBar(
+          title: const Text('Complete Registration'),
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black,),
+            onPressed: () {
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const ShopInfoScreen()));
+            },
+          ),),
+        body: ListView(
+          children: [
+            EasyStepper(
+              activeStep: activeStep,
+              lineStyle: const LineStyle(
+                  lineLength: 80,
+                  lineWidth: 2,
+                  progressColor: green1,
+                  progress: 0
+              ),
+              activeStepTextColor: Colors.black87,
+              finishedStepTextColor: Colors.black87,
+              internalPadding: 0,
+              showLoadingAnimation: false,
+              stepRadius: 8,
+              showStepBorder: false,
+              steps: [
+                EasyStep(
+                  customStep: CircleAvatar(
+                    radius: 8,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 7,
+                      backgroundColor:
+                      activeStep == 0 ? green1 : Colors.grey.shade400,
+                    ),
+                  ),
+                  title: 'Basic Information',
+                ),
+                EasyStep(
+                    customStep: CircleAvatar(
+                      radius: 8,
+                      backgroundColor: Colors.white,
+                      child: CircleAvatar(
+                        radius: 7,
+                        backgroundColor:
+                        activeStep == 1 ? green1 : Colors.grey.shade400,
+                      ),
+                    ),
+                    title: 'Shipping Information',
+                    topTitle: true
+                ),
+                EasyStep(
+                  customStep: CircleAvatar(
+                    radius: 8,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 7,
+                      backgroundColor:
+                      activeStep == 2 ? green1 : Colors.grey.shade400,
+                    ),
+                  ),
+                  title: 'Bank Account',
+                ),
+                EasyStep(
+                    customStep: CircleAvatar(
+                      radius: 8,
+                      backgroundColor: Colors.white,
+                      child: CircleAvatar(
+                        radius: 7,
+                        backgroundColor:
+                        activeStep == 3 ? green1 : Colors.grey.shade400,
+                      ),
+                    ),
+                    title: 'Complete',
+                    topTitle: true
+                )
+              ],
             ),
-            activeStepTextColor: Colors.black87,
-            finishedStepTextColor: Colors.black87,
-            internalPadding: 0,
-            showLoadingAnimation: false,
-            stepRadius: 8,
-            showStepBorder: false,
-            steps: [
-              EasyStep(
-                customStep: CircleAvatar(
-                  radius: 8,
-                  backgroundColor: Colors.white,
-                  child: CircleAvatar(
-                    radius: 7,
-                    backgroundColor:
-                    activeStep == 0 ? green1 : Colors.grey.shade400,
-                  ),
-                ),
-                title: 'Thông tin cơ bản',
-              ),
-              EasyStep(
-                  customStep: CircleAvatar(
-                    radius: 8,
-                    backgroundColor: Colors.white,
-                    child: CircleAvatar(
-                      radius: 7,
-                      backgroundColor:
-                      activeStep == 1 ? green1 : Colors.grey.shade400,
-                    ),
-                  ),
-                  title: 'Thông tin vận chuyển',
-                  topTitle: true
-              ),
-              EasyStep(
-                customStep: CircleAvatar(
-                  radius: 8,
-                  backgroundColor: Colors.white,
-                  child: CircleAvatar(
-                    radius: 7,
-                    backgroundColor:
-                    activeStep == 2 ? green1 : Colors.grey.shade400,
-                  ),
-                ),
-                title: 'Tài khoản ngân hàng',
-              ),
-              EasyStep(
-                  customStep: CircleAvatar(
-                    radius: 8,
-                    backgroundColor: Colors.white,
-                    child: CircleAvatar(
-                      radius: 7,
-                      backgroundColor:
-                      activeStep == 3 ? green1 : Colors.grey.shade400,
-                    ),
-                  ),
-                  title: 'Hoàn tất',
-                  topTitle: true
-              )
-            ],
-          ),
-          const Padding(
+            const Padding(
               padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Text('Kiểm tra thông tin của cửa hàng bạn để hoàn tất đăng ký',
-                style: TextStyle(fontSize: 16,fontWeight:FontWeight.bold),
+              child: Text('Check your shop information to complete registration',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
-          ),
-          const SizedBox(height: 10),
-          information('Tên cửa hàng: **********'),
-          information('Email: **********'),
-          information('Số điện thoại: **********'),
-          information('Địa chỉ nhận hàng: **********'),
-          information('Phương thức vận chuyển: **********'),
-          information('Số tài khoản ngân hàng: **********'),
-          CheckboxListTile(
+            ),
+            const SizedBox(height: 10),
+            information('Shop Name: **********'),
+            information('Email: **********'),
+            information('Phone Number: **********'),
+            information('Shipping Address: **********'),
+            information('Shipping Method: **********'),
+            information('Bank Account Number: **********'),
+            CheckboxListTile(
               value: onTapped,
-            checkColor: green1,
-            activeColor: Colors.white,
+              checkColor: green1,
+              activeColor: Colors.white,
               onChanged: (bool? value) {
                 setState(() {
                   onTapped = value!;
                 });
               },
-            title: const Text('Bằng cách ấn vào nút đăng ký bạn đã đồng ý với thông tin và điều khoản của Green Circle'),
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10,horizontal:60),
-            child: Container(
-              height: 50,
-              width:MediaQuery.of(context).size.width,
-              decoration:BoxDecoration(
-                  color: green1,
-                  borderRadius: BorderRadius.circular(50)
-              ),
-              child: Center(
-                  child: TextButton(
-                    child: Text("Đăng ký",style:GoogleFonts.almarai(color:Colors.white,fontSize:20,fontWeight: FontWeight.w700),),
-                    onPressed:(){
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              backgroundColor: Colors.white,
-                              title: const Text('Đăng ký thành công'),
-                              content: const Text('Chúng tôi sẽ gửi lại email để xác nhận lại thông tin thẻ ngân hàng của bạn. Cảm ơn bạn đã đăng ký với Green Circle!'),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MainScreen()));
-                                  },
-                                  child: const Text('Quay lai trang chủ',style: TextStyle(color: green1,fontWeight: FontWeight.bold))
-                                ),
-                              ],
-                            );
-                          }
-                      );
-                    },
-                  )),
+              title: const Text('By clicking the register button, you agree to the information and terms of Green Circle'),
+              controlAffinity: ListTileControlAffinity.leading,
             ),
-          ),
-        ],
-      )
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 60),
+              child: GestureDetector(
+                onTap: (){showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        backgroundColor: Colors.white,
+                        title: const Text('Sign up successfully!', style: TextStyle(color: green1, fontWeight: FontWeight.bold)),
+                        content: const Text('We will send an email to verify your bank card information. Thank you for registering with Green Circle!'),
+                        actions: <Widget>[
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MainScreen()));
+                              },
+                              child: const Text('Back to Home', style: TextStyle(color: green1, fontWeight: FontWeight.bold))
+                          ),
+                        ],
+                      );
+                    }
+                );
+                },
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: green1,
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Center(
+                      child: Text("Register", style: GoogleFonts.almarai(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),)),
+                ),
+              ),
+            ),
+          ],
+        )
     );
   }
 
-  Widget information(String content){
+  Widget information(String content) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal:30,vertical:15),
-      child:Container(
-        height:45,
-        width: 350,
-        decoration: BoxDecoration(
-          color: lightGray,
-          border: Border.all(color: Colors.black87),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          children: [
-            const SizedBox(width: 15,),
-            Text(content,style: const TextStyle(fontSize: 16,fontWeight: FontWeight.bold),textAlign: TextAlign.start,)
-            ]
-        ),
-      )
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+        child: Container(
+          height: 45,
+          width: 350,
+          decoration: BoxDecoration(
+            color: lightGray,
+            border: Border.all(color: Colors.black87),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+              children: [
+                const SizedBox(width: 15,),
+                Text(content, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold), textAlign: TextAlign.start,)
+              ]
+          ),
+        )
     );
   }
 }
